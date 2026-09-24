159f037eb82c33b7709bfdd1c4984a1a89d7af70
Robby Thompson <robsdudeson@gmail.com>
2026-09-23 23:23:55 -0500

    test: fix extension test runner

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,13 +1,27 @@
-import { runTests } from '@vscode/test-electron'
+import { downloadAndUnzipVSCode, runTests } from '@vscode/test-electron'
+import * as fs from 'fs'
 import * as path from 'path'
 
 import { USER_DATA_DIR, WORKSPACE_DIR } from './suite/setup'
+
+async function resolveVSCodeExecutablePath (): Promise<string> {
+  const configuredExecutablePath = process.env.VSCODE_TEST_EXECUTABLE_PATH ?? process.env.VSCODE_EXECUTABLE_PATH
+  if (configuredExecutablePath != null && configuredExecutablePath.length > 0) return configuredExecutablePath
+
+  const downloadedExecutablePath = await downloadAndUnzipVSCode()
+  const codeExecutablePath = downloadedExecutablePath.replace(/\/Electron$/, '/Code')
+  if (fs.existsSync(codeExecutablePath)) return codeExecutablePath
+  if (fs.existsSync(downloadedExecutablePath)) return downloadedExecutablePath
+
+  return downloadedExecutablePath
+}
 
 async function main (): Promise<void> {
   try {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../')
     const extensionTestsPath = path.resolve(__dirname, './suite/index')
-    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ['--disable-extensions', '--disable-gpu', '--user-data-dir', USER_DATA_DIR, WORKSPACE_DIR] })
+    const vscodeExecutablePath = await resolveVSCodeExecutablePath()
+    await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath, launchArgs: ['--disable-extensions', '--disable-gpu', '--user-data-dir', USER_DATA_DIR, WORKSPACE_DIR] })
   } catch (err) {
     console.error('Failed to run tests')
     process.exit(1)
